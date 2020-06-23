d3d4a5cb2307563b1f1953b03fa2f4e0634f2ba8
Shivam <singhal29797shivam@gmail.com>
2020-06-24 04:09:55 +0530

    first checkpoint commit

---
+++ src/main/java/com/upgrad/hirewheels/client/App.java
@@ -0,0 +1,19 @@
+package com.upgrad.hirewheels.client;
+
+import com.upgrad.hirewheels.practice.BalanceService;
+import com.upgrad.hirewheels.practice.Users;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        Users users = (Users) context.getBean("user1");
+        System.out.println(users);
+
+        BalanceService balanceService = (BalanceService) context.getBean("userBalance");
+        System.out.println(balanceService.checkUserBalance());
+
+
+    }
+}
